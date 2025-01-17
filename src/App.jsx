956b667bfe017a49c0a430dc40105e160c01ba8b
devtuanamit1409 <nguyentuanamit@gmail.com>
2025-01-17 15:30:02 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
